02eafef6fa88015cff3a5324805a64e5744a59d9
Nicole Forke <n.forke@gmail.com>
2020-06-20 11:40:21 -0500

    Adding commit for week-8

---
+++ week-8/forke_hello_world.py
@@ -0,0 +1,12 @@
+#/*
+#================================
+#; Title: Assignment 8.2
+#; Author: Nicole Forke
+#; Date: 20 June 2020
+#; Modified By: Nicole Forke
+#; Description: Anaconda
+#================================
+#*/
+
+#// print hello world using python
+print('Hello World')
