28c624525abaafcda8ce86de270919b3ee8c6951
Nicole Forke <n.forke@gmail.com>
2020-06-20 12:10:23 -0500

    Adding commit for week-8

---
+++ week-8/forke_calculator.py
@@ -0,0 +1,26 @@
+#/*
+#================================
+#; Title: Assignment 8.3
+#; Author: Nicole Forke
+#; Date: 20 June 2020
+#; Modified By: Nicole Forke
+#; Description: Python in Action
+#================================
+#*/
+
+# add function with two parameters
+def add(x, y):
+  return x + y
+
+# subtract function with two parameters
+def subtract(z, x):
+  return z - x
+
+# divide function with two parameters
+def divide(z, y):
+  return z / y
+
+# print statements to call each function
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
